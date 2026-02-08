0eaaee95a14295dd97ce49b2abb20b784bc0f42e
Harun Spago <harun.spago.code@gmail.com>
2026-02-08 22:23:05 +0100

    modules:telemetry: Initial commit of telemetry module
    Signed-off-by: Harun Spago <harun.spago.code@gmail.com>

---
+++ modules/telemetry/zephyr/include/telemetry/communication_interface.h
@@ -0,0 +1,30 @@
+#ifndef COMMUNICATION_INTERFACE_H
+#define COMMUNICATION_INTERFACE_H
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <stddef.h>
+
+typedef void  (*comm_recv_fn)(const struct device *dev, void *user_data);
+typedef void (*comm_send_fn)(const struct device *dev, const char *data, size_t len);
+
+struct telemetry_comm_interface {
+    struct device *dev;
+    comm_recv_fn recv;
+    comm_send_fn send;
+};
+
+struct registered_telemetry_senders_t {
+    const struct telemetry_comm_interface* interfaces[4]; // Hard coded for now
+    size_t count;
+};
+
+extern struct registered_telemetry_senders_t registered_telemetry_senders;
+
+static inline void register_telemetry_sender(const struct telemetry_comm_interface *iface) {
+    if (registered_telemetry_senders.count < 4) {
+        registered_telemetry_senders.interfaces[registered_telemetry_senders.count++] = iface;
+    }
+}
+
+#endif // COMMUNICATION_INTERFACE_H
